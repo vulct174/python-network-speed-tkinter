55c534c10ca65a2e27b67e856d9a8a68084186a9
vulct <vu237362@gmail.com>
2024-05-21 16:17:42 +0700

    feat: create logic with thread

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import tkinter as tk
+import speedtest
+import threading
+
+
+def check_network_speed():
+    check_button.config(state=tk.DISABLED)
+    check_button.config(text="Đang kiểm tra...")
+
+    def check_speed():
+        speed_test = speedtest.Speedtest()
+        speed_test.get_best_server()
+
+        download_speed = speed_test.download() / 1024 / 1024
+        upload_speed = speed_test.upload() / 1024 / 1024
+
+        download_result.config(text=f"{round(download_speed, 2)} Mbps")
+        upload_result.config(text=f"{round(upload_speed, 2)} Mbps")
+
+        check_button.config(state=tk.NORMAL)
+        check_button.config(text="Kiểm tra tốc độ")
+
+    thread = threading.Thread(target=check_speed)
+    thread.start()
+
 
 root = tk.Tk()
 root.title("Check network speed")
@@ -25,7 +49,7 @@ upload_result = tk.Label(frame, text="0 Mbps", font=('Helvetica', 12), bg='green
 upload_result.pack(pady=7)
 
 check_button = tk.Button(frame, text="Kiểm tra tốc độ", font=('Helvetica', 12), bg='white', fg='green', width=50,
-                         height=2)
+                         height=2, command=check_network_speed)
 check_button.pack(pady=7)
 
 copyright_label = tk.Label(root, text="Copyright@vu.lct", font=('Helvetica', 10), bg='green', fg='white')
