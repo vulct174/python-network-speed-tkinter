d6e764810fd14bb40816db15ead37935d8a86970
vulct <vu237362@gmail.com>
2024-05-21 16:22:14 +0700

    style: create common style

--- main.py
+++ main.py
@@ -32,27 +32,29 @@ root.geometry("500x500")
 frame = tk.Frame(root, bg='green')
 frame.pack(padx=50, pady=50)
 
-download_label = tk.Label(frame, text="Tốc độ download", font=('Helvetica', 12), bg='green', fg='white', width=40,
-                          height=2, highlightthickness=1)
+label_style = {'font': ('Helvetica', 12), 'bg': 'green', 'fg': 'white', 'width': 40, 'height': 2,
+               'highlightthickness': 1}
+
+button_style = {'font': ('Helvetica', 12), 'bg': 'white', 'fg': 'green', 'width': 50, 'height': 2}
+
+copyright_style = {'font': ('Helvetica', 10), 'bg': 'green', 'fg': 'white', 'border': 0}
+
+download_label = tk.Label(frame, text="Tốc độ download", **label_style)
 download_label.pack(pady=7)
 
-download_result = tk.Label(frame, text="0 Mbps", font=('Helvetica', 12), bg='green', fg='white', width=40, height=2,
-                           highlightthickness=1)
+download_result = tk.Label(frame, text="0 Mbps", **label_style)
 download_result.pack(pady=7)
 
-upload_label = tk.Label(frame, text="Tốc độ upload", font=('Helvetica', 12), bg='green', fg='white', width=40,
-                        height=2, highlightthickness=1)
+upload_label = tk.Label(frame, text="Tốc độ upload", **label_style)
 upload_label.pack(pady=7)
 
-upload_result = tk.Label(frame, text="0 Mbps", font=('Helvetica', 12), bg='green', fg='white', width=40, height=2,
-                         highlightthickness=1)
+upload_result = tk.Label(frame, text="0 Mbps", **label_style)
 upload_result.pack(pady=7)
 
-check_button = tk.Button(frame, text="Kiểm tra tốc độ", font=('Helvetica', 12), bg='white', fg='green', width=50,
-                         height=2, command=check_network_speed)
+check_button = tk.Button(frame, text="Kiểm tra tốc độ", command=check_network_speed, **button_style)
 check_button.pack(pady=7)
 
-copyright_label = tk.Label(root, text="Copyright@vu.lct", font=('Helvetica', 10), bg='green', fg='white')
+copyright_label = tk.Label(root, text="Copyright@vu.lct", **copyright_style)
 copyright_label.pack(pady=7)
 
 root.mainloop()
